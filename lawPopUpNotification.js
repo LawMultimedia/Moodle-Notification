ec8de131db16693a415dfa37fd892a182d06f867
LawMultimedia <85203163+LawMultimedia@users.noreply.github.com>
2021-06-07 16:29:04 +1000

    Update lawPopUpNotification.js

--- lawPopUpNotification.js
+++ lawPopUpNotification.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
 // grab the unit title out of the image/banner header at the top of the site, remove all the spaces in the name, and take the first 3 characters.
 var headerTitle = $('.header-title').text().replace(/\s/g,'').slice(0,3);
 // define expiration duration as 144 hours/7 days
-	const expirationDuration = 1000 * 60 * 60 * 144;
+	const expirationDuration = 1000 * 60 * 60 * 288;
 // save the time of the current login to localStorage
 	const savedTime = localStorage.getItem('savedTime');
 // get the time of the current login
@@ -12,7 +12,7 @@ var headerTitle = $('.header-title').text().replace(/\s/g,'').slice(0,3);
 // make a constant that refers to when the login has a history, and meets the requirements to display the notification again
 	const AcceptedExpired = savedTime != undefined && currentTime - savedTime > expirationDuration;
 // Attendance requirements notification
-	$('.sectionname').before('<div class="container"><div class="modal fade" id="attNotification" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Attendance expectations</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><p>The Arts Faculty has an expectation that you attend all scheduled workshops and tutorials, and participate in all learning activities. All the evidence suggests that student success is greatly impacted by class attendance and participation.</p><p>If you are unable to attend a scheduled workshop or tutorial, please contact your tutor or unit coordinator, and ensure you have strategies in place to catch up on any missed work.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>');
+	$('.sectionname').before('<div class="container"><div class="modal fade" id="attNotification" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Attendance expectations</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><p>The Faculty of Law has an expectation that you attend all classes and participate in all learning activities. All the evidence suggests that student academic success is greatly impacted by class attendance and participation</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>');
 // if the unit has LAW in its title and its either the first visit, or their first visit since the user has gone over the expirationDuration time
 	if (headerTitle.indexOf("LAW") != -1){
  		if(notAccepted || AcceptedExpired){
